885adf479c2621d59343cdadd5061746c9b4b6a9
tharindu326 <tharindu326@gmail.com>
2022-06-03 15:13:59 +0530

    re-structure util functions

--- image_inference.py
+++ image_inference.py
@@ -32,8 +32,8 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
-from utils.general import check_img_size, xywh2xyxy
 from utils.torch_utils import select_device
+from utils.general import non_max_suppression, scale_coords, check_img_size
 
 
 class Colors:
@@ -82,36 +82,36 @@ class InferenceYOLOV5:
         self.FP16_half_precision = cfg.inferYOLOV5.half  # use FP16 half-precision inference
         self.dnn = cfg.inferYOLOV5.dnn  # use OpenCV DNN for ONNX inference
 
+        self.colors = Colors()  # create instance for 'from utils.plots import colors'
+        # Load model
+        self.device = select_device(self.device_gpu)
+        self.model = DetectMultiBackend(self.weight_file, device=self.device, dnn=self.dnn, data=self.data,
+                                   fp16=self.FP16_half_precision)
+        self.stride, self.names, self.pt = self.model.stride, self.model.names, self.model.pt
+        imgsz = check_img_size(self.imgsz, s=self.stride)  # check image size
+
     @torch.no_grad()
     def inferYOLOV5(self, img0):
 
-        colors = Colors()  # create instance for 'from utils.plots import colors'
-        # Load model
-        device = select_device(self.device_gpu)
-        model = DetectMultiBackend(self.weight_file, device=device, dnn=self.dnn, data=self.data,
-                                   fp16=self.FP16_half_precision)
-        stride, names, pt = model.stride, model.names, model.pt
-        imgsz = check_img_size(self.imgsz, s=stride)  # check image size
-
         # img0 = cv2.imread(image)
-        img = self.letterbox(img0, imgsz, stride=stride, auto=pt)[0]
+        img = self.letterbox(img0, self.imgsz, stride=self.stride, auto=self.pt)[0]
 
         # Convert
         img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
         img = np.ascontiguousarray(img)
 
         # Run inference
-        model.warmup(imgsz=(1, 3, *imgsz))  # warmup
-        im = torch.from_numpy(img).to(device)
-        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        self.model.warmup(imgsz=(1, 3, *self.imgsz))  # warmup
+        im = torch.from_numpy(img).to(self.device)
+        im = im.half() if self.model.fp16 else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
 
         # Inference
-        pred = model(im)
+        pred = self.model(im)
         # NMS
-        pred = self.non_max_suppression(pred, self.confidence_threshold, self.nms_iou_threshold, self.classes,
+        pred = non_max_suppression(pred, self.confidence_threshold, self.nms_iou_threshold, self.classes,
                                         self.agnostic_nms, max_det=self.max_detections)
 
         # Second-stage classifier (optional)
@@ -126,7 +126,7 @@ class InferenceYOLOV5:
         for i, det in enumerate(pred):  # per image
             if len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = self.scale_coords(im.shape[2:], det[:, :4], img0.shape).round()
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], img0.shape).round()
                 for *xyxy, conf, cls in reversed(det):
                     scores.append(float(f'{conf}'))
                     box = xyxy
@@ -138,10 +138,10 @@ class InferenceYOLOV5:
                     c = int(cls)  # integer class
                     class_ids.append(c)
                     label = None if self.hide_labels else (
-                        names[c] if self.hide_confidence else f'{names[c]} {conf:.2f}')
-                    class_names.append(names[c])
+                        self.names[c] if self.hide_confidence else f'{self.names[c]} {conf:.2f}')
+                    class_names.append(self.names[c])
                     p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
-                    color = colors(c, True)
+                    color = self.colors(c, True)
                     lw = 3 or max(round(sum(img0.shape) / 2 * 0.003), 2)  # line width
                     if label and self.overlay:
                         cv2.rectangle(img0, p1, p2, color, thickness=lw, lineType=cv2.LINE_AA)
@@ -194,154 +194,6 @@ class InferenceYOLOV5:
         left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
         im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
         return im, ratio, (dw, dh)
-
-    def non_max_suppression(self, prediction,
-                            conf_thres=0.25,
-                            iou_thres=0.45,
-                            classes=None,
-                            agnostic=False,
-                            multi_label=False,
-                            labels=(),
-                            max_det=300):
-        """Non-Maximum Suppression (NMS) on inference results to reject overlapping bounding boxes
-
-        Returns:
-             list of detections, on (n,6) tensor per image [xyxy, conf, cls]
-        """
-
-        bs = prediction.shape[0]  # batch size
-        nc = prediction.shape[2] - 5  # number of classes
-        xc = prediction[..., 4] > conf_thres  # candidates
-
-        # Checks
-        assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
-        assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
-
-        # Settings
-        # min_wh = 2  # (pixels) minimum box width and height
-        max_wh = 7680  # (pixels) maximum box width and height
-        max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
-        redundant = True  # require redundant detections
-        multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-        merge = False  # use merge-NMS
-
-        output = [torch.zeros((0, 6), device=prediction.device)] * bs
-        for xi, x in enumerate(prediction):  # image index, image inference
-            # Apply constraints
-            # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
-            x = x[xc[xi]]  # confidence
-
-            # Cat apriori labels if autolabelling
-            if labels and len(labels[xi]):
-                lb = labels[xi]
-                v = torch.zeros((len(lb), nc + 5), device=x.device)
-                v[:, :4] = lb[:, 1:5]  # box
-                v[:, 4] = 1.0  # conf
-                v[range(len(lb)), lb[:, 0].long() + 5] = 1.0  # cls
-                x = torch.cat((x, v), 0)
-
-            # If none remain process next image
-            if not x.shape[0]:
-                continue
-
-            # Compute conf
-            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
-
-            # Box (center x, center y, width, height) to (x1, y1, x2, y2)
-            box = xywh2xyxy(x[:, :4])
-
-            # Detections matrix nx6 (xyxy, conf, cls)
-            if multi_label:
-                i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
-                x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
-            else:  # best class only
-                conf, j = x[:, 5:].max(1, keepdim=True)
-                x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
-
-            # Filter by class
-            if classes is not None:
-                x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
-
-            # Apply finite constraint
-            # if not torch.isfinite(x).all():
-            #     x = x[torch.isfinite(x).all(1)]
-
-            # Check shape
-            n = x.shape[0]  # number of boxes
-            if not n:  # no boxes
-                continue
-            elif n > max_nms:  # excess boxes
-                x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
-
-            # Batched NMS
-            c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
-            boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-            i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-            if i.shape[0] > max_det:  # limit detections
-                i = i[:max_det]
-            if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
-                # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-                iou = self.box_iou(boxes[i], boxes) > iou_thres  # iou matrix
-                weights = iou * scores[None]  # box weights
-                x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
-                if redundant:
-                    i = i[iou.sum(1) > 1]  # require redundancy
-
-            output[xi] = x[i]
-
-        return output
-
-    def scale_coords(self, img1_shape, coords, img0_shape, ratio_pad=None):
-        # Rescale coords (xyxy) from img1_shape to img0_shape
-        if ratio_pad is None:  # calculate from img0_shape
-            gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
-            pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
-        else:
-            gain = ratio_pad[0][0]
-            pad = ratio_pad[1]
-
-        coords[:, [0, 2]] -= pad[0]  # x padding
-        coords[:, [1, 3]] -= pad[1]  # y padding
-        coords[:, :4] /= gain
-        self.clip_coords(coords, img0_shape)
-        return coords
-
-    @staticmethod
-    def clip_coords(boxes, shape):
-        # Clip bounding xyxy bounding boxes to image shape (height, width)
-        if isinstance(boxes, torch.Tensor):  # faster individually
-            boxes[:, 0].clamp_(0, shape[1])  # x1
-            boxes[:, 1].clamp_(0, shape[0])  # y1
-            boxes[:, 2].clamp_(0, shape[1])  # x2
-            boxes[:, 3].clamp_(0, shape[0])  # y2
-        else:  # np.array (faster grouped)
-            boxes[:, [0, 2]] = boxes[:, [0, 2]].clip(0, shape[1])  # x1, x2
-            boxes[:, [1, 3]] = boxes[:, [1, 3]].clip(0, shape[0])  # y1, y2
-
-    @staticmethod
-    def box_area(box):
-        # box = xyxy(4,n)
-        return (box[2] - box[0]) * (box[3] - box[1])
-
-    def box_iou(self, box1, box2):
-        # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
-        """
-        Return intersection-over-union (Jaccard index) of boxes.
-        Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
-        Arguments:
-            box1 (Tensor[N, 4])
-            box2 (Tensor[M, 4])
-        Returns:
-            iou (Tensor[N, M]): the NxM matrix containing the pairwise
-                IoU values for every element in boxes1 and boxes2
-        """
-
-        # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
-        (a1, a2), (b1, b2) = box1[:, None].chunk(2, 2), box2.chunk(2, 1)
-        inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp(0).prod(2)
-
-        # IoU = inter / (area1 + area2 - inter)
-        return inter / (self.box_area(box1.T)[:, None] + self.box_area(box2.T) - inter)
 
 
 if __name__ == "__main__":
